97b64ef69f66cee7788f478b12801405135edb0c
HazanOmri <omrihazan1313@gmail.com>
2023-08-08 20:31:27 +0300

    Atlas connection

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  dbURL: 'mongodb+srv://omrihazan13:omrihazan13@cluster0.loaaeff.mongodb.net/?retryWrites=true&w=majority',
+  dbURL: 'mongodb+srv://omrihazan13:omrihazan13@cluster0.xmgzrku.mongodb.net/?retryWrites=true&w=majority',
   // dbURL: 'mongodb://127.0.0.1:27017',
 
   dbName: 'market_db'
